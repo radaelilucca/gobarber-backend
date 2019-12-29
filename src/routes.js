f74cb2d3121141a29234eadaa3e8dd213d6ec7c8
radaelilucca <luccaradaeli@gmail.com>
2019-12-29 16:34:08 -0300

    add avatar to providers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
@@ -18,8 +19,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
