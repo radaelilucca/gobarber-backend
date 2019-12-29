af6365f21a8cf6c1f74046d13cc15f6c46fd7ad6
radaelilucca <luccaradaeli@gmail.com>
2019-12-29 20:40:51 -0300

    create appointments

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,43 @@
+import * as yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      date: yup.date().required(),
+      provider_id: yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'VALIDATION FAILS' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    // check if provider_id is a provider
+
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 // Import configs and middlewares
 import multerConfig from './config/multer';
@@ -26,5 +27,6 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
