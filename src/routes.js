094480f1daf6a870dd22f3ab1915b8d0d870c3b4
radaelilucca <luccaradaeli@gmail.com>
2019-12-30 13:09:32 -0300

    feat- schedule for logged provider

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 // Import configs and middlewares
 import multerConfig from './config/multer';
@@ -31,5 +32,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
