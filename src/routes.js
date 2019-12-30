3841cc013a115b92b1c149c5dbab0fbbd9cdab5e
radaelilucca <luccaradaeli@gmail.com>
2019-12-30 14:27:36 -0300

    Mark a notification as read

--- src/routes.js
+++ src/routes.js
@@ -37,5 +37,6 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
