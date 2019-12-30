3d1621164c02cbcbf75d13d8f07a904b014303d2
radaelilucca <luccaradaeli@gmail.com>
2019-12-29 21:02:10 -0300

    check past dates and availability for appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -28,6 +29,31 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    // check for past dates
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(401).json({
+        error: 'Past dates are not allowed',
+      });
+    }
+
+    // check date availability
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment Date is no available' });
     }
 
     const appointment = await Appointment.create({
