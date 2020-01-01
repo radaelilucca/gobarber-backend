a49904f8c7a040ac1ac8c7fe62892941f4902fb9
radaelilucca <luccaradaeli@gmail.com>
2020-01-01 17:58:39 -0300

    feat- exception handlers with sentry.io and Youch

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -20,7 +20,7 @@ class AvailableController {
 
     const searchDate = Number(date);
 
-    const appointments = await Appointment.findAll({
+    const appointments = await Appointment.findaaaaaaaaaaaaaaAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://346e78c0ec8345e9a60f8d10e04ab60e@sentry.io/1869827',
+};
